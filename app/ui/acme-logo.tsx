636c0b8f299118a9f98c144d5cbb3b036aa3cbf3
Krishanx2003 <aiml21040@glbitm.ac.in>
2024-07-25 15:22:37 +0530

    font error

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,13 +1,12 @@
 import { GlobeAltIcon } from '@heroicons/react/24/outline';
-import { lusitana } from '@/app/ui/fonts';
+import  './global.css'
 
 export default function AcmeLogo() {
   return (
-    <div
-      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
-    >
-      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <p className="text-[44px]">Acme</p>
-    </div>
+    <div className="lusitana-regular flex flex-row items-center leading-none text-white">
+    <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
+    <p className="text-[44px] lusitana-bold">Acme</p>
+  </div>
+
   );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+// @/app/ui/fonts.ts
+import localFont from 'next/font/local';
+
+export const lusitana = localFont({
+  src: './path-to-font-file.woff2',
+});
