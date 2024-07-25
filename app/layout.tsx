a4d88eab2b8852fefe5afd568a2d3cb7c66a3591
Krishanx2003 <aiml21040@glbitm.ac.in>
2024-07-25 15:55:08 +0530

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,19 @@
-import '../app/ui/global.css'
-import { Inter } from 'next/font/google';
+import { Inter } from 'next/font/google'
  
-export const inter = Inter({ subsets: ['latin'] });
-
+// If loading a variable font, you don't need to specify the font weight
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+})
+ 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-       <body className={`${inter.className} antialiased`}>{children}</body>
+    <html lang="en" className={inter.className}>
+      <body>{children}</body>
     </html>
-  );
+  )
 }
